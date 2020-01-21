448e5cfef6d50f5533eb013c1874b9975407ae67
Karl Kowalczyk <karl1112@gmail.com>
2020-01-20 21:39:26 -0700

    added required field validation to the forms

--- hktm/lesson_contents/forms.py
+++ hktm/lesson_contents/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired
 
 ## this form witll b e used for adding and editing
 class AddForm(FlaskForm):
-    name = StringField(u'ページタイトルを記載してください')
+    name = StringField(u'ページタイトルを記載してください',validators=[DataRequired()])
     content = TextAreaField(u'問題を作成してください')
     submit = SubmitField(u'保存')
--- hktm/lessons/forms.py
+++ hktm/lessons/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField, IntegerField
+from wtforms.validators import InputRequired
 
 ## this form witll b e used for adding and editing
 class AddForm(FlaskForm):
-    name = StringField(u'学期と単元名')
+    name = StringField(u'学期と単元名',validators=[InputRequired(message='This field is required!!!!!')])
     grade = SelectField(u'Grade', choices=[])
     submit = SubmitField(u'保存する')
 
 class MaterialForm(FlaskForm):
-    mat_name = StringField(u'Enter the name of the sheet - this will be on the printerd document')
+    mat_name = StringField(u'Enter the name of the sheet - this will be on the printerd document',validators=[InputRequired(message='This field is required!!!!!')])
     mat_content = TextAreaField(u'Here are the questions for this material')
     mat_type = SelectField(u'Content Type', choices=[('KJTS','Kanji Test'),('WRKS','Worksheet'),('KJPR','Kanji Practice')])
     mat_submit = SubmitField(u'Save')
