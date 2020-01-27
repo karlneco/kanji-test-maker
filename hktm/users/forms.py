c4f7136f11f3878d161154bb20c1e52c8ff72526
Karl Kowalczyk <karl1112@gmail.com>
2020-01-27 01:52:09 -0700

    user management framework is in place, needs testing

--- hktm/users/forms.py
+++ hktm/users/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired
 from wtforms import ValidationError
 
 class LoginForm(FlaskForm):
@@ -8,9 +8,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     submit = SubmitField('Log In')
 
+class AdminAddForm(FlaskForm):
+    name = StringField('Teacher Name')
+    email = StringField('Email', validators=[InputRequired(message='This field is required!!!!!'),Email()])
+    password = StringField('Password', validators=[InputRequired(message='This field is required!!!!!')])
+    grades = StringField('Grades', validators=[InputRequired(message='This field is required!!!!!')])
+    submit = SubmitField('Save')
+
+class AdminEditForm(FlaskForm):
+    name = StringField('Teacher Name')
+    email = StringField('Email', validators=[InputRequired(message='This field is required!!!!!'),Email()])
+    password = StringField('Password')
+    grades = StringField('Grades', validators=[InputRequired(message='This field is required!!!!!')])
+    submit = SubmitField('Save')
+
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(),Email()])
+    email = StringField('Email', validators=[InputRequired(message='This field is required!!!!!'),Email()])
     password = PasswordField('Password',validators=[DataRequired(),EqualTo('password_confirm', message='Passwords must match')])
     password_confirm = PasswordField('Re-Enter Password',validators=[DataRequired()])
     submit = SubmitField('Register')
--- hktm/users/views.py
+++ hktm/users/views.py
@@ -2,9 +2,90 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, current_user, logout_user, login_required
 from hktm import db
 from hktm.models import User
-from hktm.users.forms import LoginForm, RegistrationForm
+from hktm.users.forms import LoginForm, RegistrationForm, AdminEditForm, AdminAddForm
 
 users_bp = Blueprint('users', __name__, template_folder='templates/users')
+
+
+@users_bp.route('/list')
+@login_required
+def admin_list_users():
+    if 'A' not in current_user.grades:
+        flash('You do not have the Administrator access required to modify users.')
+        return redirect(url_for('root.home'))
+
+    all_users = db.session.query(User).all()
+    return render_template('list.html', users=all_users)
+
+@users_bp.route('/edit/<int:id>', methods=['GET','POST'])
+@login_required
+def admin_user_edit(id):
+    if 'A' not in current_user.grades:
+        flash('You do not have the Administrator access required to modify users.')
+        return redirect(url_for('root.home'))
+
+    user = User.query.get(id)
+    form = AdminEditForm()
+
+    if request.method=='GET':
+        form.name.default = user.name
+        form.email.default = user.email
+        form.grades.default = user.grades
+        form.process()
+
+    if form.validate_on_submit():
+        user.name = form.name.data
+        user.grades = form.grades.data
+        if form.password.data != "":
+            user.password = generate_password_hash(form.password.data)
+
+        db.session.commit()
+        return redirect(url_for('users.admin_list_users'))
+    else:
+        form.name.default = user.name
+        form.email.default = user.email
+        form.grades.default = user.grades
+        form.process()        
+
+    return render_template('admin_user_edit.html',form=form,user_id=user.id)
+
+    user.email = form.email.data
+
+@users_bp.route('/add', methods=['GET','POST'])
+@login_required
+def add():
+    if 'A' not in current_user.grades:
+        flash('You do not have the Administrator access required to modify users.')
+        return redirect(url_for('root.home'))
+
+    form = AdminAddForm()
+    if form.validate_on_submit():
+        new_user = User(form.email.data, form.password.data)
+        new_user.name = form.name.data
+        new_user.grades = form.grades.data
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('users.admin_list_users'))
+
+    return render_template('admin_user_add.html',form=form)
+
+@users_bp.route('/delete/<int:id>', methods=['GET','POST'])
+@login_required
+def delete(id):
+    if 'A' not in current_user.grades:
+        flash('You do not have the Administrator access required to modify users.')
+        return redirect(url_for('root.home'))
+
+    user_to_delete = User.query.get(id)
+
+    user_email = user_to_delete.email
+    user_name = user_to_delete.name
+    flash(f'User {user_email} ({user_name}) deleted.')
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    return redirect(url_for('users.admin_list_users'))
 
 @users_bp.route('/register', methods=['GET','POST'])
 def register():
@@ -30,6 +111,7 @@ def register():
 
 
 @users_bp.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('root.index'))
